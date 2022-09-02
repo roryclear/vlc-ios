7b8bac5af3254fb2dc6f684b410526810664d534
Rory Clear <roryclear.rc@gmail.com>
2022-09-02 13:01:26 +0100

    make it run on my iphone and add blank watchOS app

---
+++ VLC-watchOS WatchKit Extension/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  VLC-watchOS WatchKit Extension
+//
+//  Created by Rory Clear on 02/09/2022.
+//  Copyright © 2022 VideoLAN. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
