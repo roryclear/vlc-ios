75d9dc9e9d0159fb6a07fa304813647597ef0cbf
Rory Clear <roryclear.rc@gmail.com>
2022-09-02 13:09:04 +0100

    add artist and album songs views

---
+++ VLC-watchOS WatchKit Extension/AlbumSongsView.swift
@@ -0,0 +1,9 @@
+//
+//  AlbumSongsView.swift
+//  VLC-watchOS WatchKit Extension
+//
+//  Created by Rory Clear on 02/09/2022.
+//  Copyright © 2022 VideoLAN. All rights reserved.
+//
+
+import Foundation
---
+++ VLC-watchOS WatchKit Extension/ArtistSongsView.swift
@@ -0,0 +1,9 @@
+//
+//  ArtistSongsView.swift
+//  VLC-watchOS WatchKit Extension
+//
+//  Created by Rory Clear on 02/09/2022.
+//  Copyright © 2022 VideoLAN. All rights reserved.
+//
+
+import Foundation
